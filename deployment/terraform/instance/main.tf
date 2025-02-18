e23831497ca2c9e64df954575c1d49717cd17d7f
Sam Warner <sam-w@users.noreply.github.com>
2025-02-18 15:45:12 +1100

    local terraform deployment
    comments

---
+++ deployment/terraform/instance/main.tf
@@ -0,0 +1,92 @@
+data "google_compute_default_service_account" "default" {
+  count = var.service_account_email == null ? 1 : 0
+
+  project = var.project
+}
+
+resource "google_compute_address" "static_ip" {
+  name         = var.instance_name
+  project      = var.project
+  region       = var.region
+  address_type = "EXTERNAL"
+
+  lifecycle {
+    # The static external IP for this instance has probably been used in CloudFlare to configure DNS for the subdomain, so terraform is configured to stop you from destroying it.
+    #
+    # This will block you from destroying the module. You can work around this protection by running:
+    # > terraform state rm module.{module_name}.google_compute_address.static_ip
+    # > terraform destroy -target module.{module_name}
+    #
+    # If you then want to recreate the module, run:
+    # > terraform import module.{module_name}.google_compute_address.static_ip {module_name}
+    # > terraform apply
+    #
+    # If you want to permanently destroy the static IP, either comment out `prevent_destroy` below, or use `terraform state rm` as shown above and manually delete the static IP.
+    prevent_destroy = true
+  }
+}
+
+resource "google_compute_instance" "instance" {
+  name    = var.instance_name
+  project = var.project
+  zone    = var.zone
+
+  machine_type   = "n1-standard-8"
+  enable_display = false
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12-bookworm-v20250113"
+      size  = 250
+    }
+  }
+  guest_accelerator {
+    count = 1
+    type  = "nvidia-tesla-t4"
+  }
+
+  # See https://cloud.google.com/compute/docs/gpus/install-drivers-gpu#install-script
+  metadata_startup_script = <<-EOT
+  #!/bin/bash
+  if test -f /opt/google/cuda-installer
+  then
+    exit
+  fi
+
+  mkdir -p /opt/google/cuda-installer
+  cd /opt/google/cuda-installer/ || exit
+
+  curl -fSsL -O https://github.com/GoogleCloudPlatform/compute-gpu-installation/releases/download/cuda-installer-v1.1.0/cuda_installer.pyz
+  python3 cuda_installer.pyz install_cuda
+  EOT
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.static_ip.address
+    }
+  }
+  tags = ["http-server", "https-server"]
+
+  service_account {
+    email = coalesce(var.service_account_email, data.google_compute_default_service_account.default.0.email)
+    scopes = [
+      # default scopes
+      "storage-ro",
+      "logging-write",
+      "monitoring-write",
+      "pubsub",
+      "service-management",
+      "service-control",
+      "trace",
+    ]
+  }
+
+  scheduling {
+    on_host_maintenance = "TERMINATE"
+  }
+  allow_stopping_for_update = true
+}
+
+locals {
+  public_ip = google_compute_instance.instance.network_interface[0].access_config[0].nat_ip
+}
